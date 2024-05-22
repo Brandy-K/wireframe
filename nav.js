826ed71df26c7fdb5cfdea87b38d9021ab65e215
Brandy-K <146661558+Brandy-K@users.noreply.github.com>
2024-05-22 21:13:59 +0200

    nav.js

--- nav.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("nav").style.padding = "20px 10px";
-  } else {
-    document.getElementById("nav").style.padding = "30px 10px";
-  }
-}
